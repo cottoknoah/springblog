0ffca6d14d508648a89ce8943d532624f3009229
Knoah Cotto <cotto.knoah@gmail.com>
2020-07-31 12:02:56 -0500

    finished exercise question 3

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,23 +1,37 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
-    /*
-    This controller should return strings for the
-    following routes that describe what will ultimately be there.
-    */
-
-    @GetMapping("/hello/{name}")
+    @GetMapping("/posts")
     @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public String postsIndex() {
+        return "posts index page";
     }
 
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postById(@PathVariable long id) {
+        return "view an individual post";
+    }
 
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postCreateForm() {
+        return "view the form for creating a post";
+    }
 
-
-
+//    connect to the first one but differently?
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postCreate() {
+        return "create a new post";
+    }
 }
+
