9e2f99e5d5de7ea420ae18459d7cada02ce530cf
Knoah Cotto <cotto.knoah@gmail.com>
2020-08-03 09:23:24 -0500

    notes from class added

--- src/main/java/com/codeup/springblog/LoginController.java
+++ src/main/java/com/codeup/springblog/LoginController.java
@@ -6,32 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String loginForm(){
+    public String viewloginForm(){
         return "login";
     }
+
 //    @PostMapping("/login")
-//    public  String viewLoginInfo(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model){
+//    public  String viewLoginInfo(@RequestParam(name = "username")
+//    String username, @RequestParam(name = "password") String password, Model model){
 //        model.addAttribute("username", username);
 //        model.addAttribute("password", password);
 //        return "login";
 //    }
-
-    @PostMapping("/login")
-    public  String viewLoginInfo(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model){
-        ArrayList<String> ads = new ArrayList<String>();
-        ads.add("Ad 1");
-        ads.add("Ad 2");
-        ads.add("Ad 3");
-
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        return "login";
-    }
 
 
 }
