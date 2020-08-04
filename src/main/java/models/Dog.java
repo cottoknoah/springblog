41c97dbc5c2a55a64c19a14cd840ae4015d97cd8
Knoah Cotto <cotto.knoah@gmail.com>
2020-08-03 20:21:06 -0500

    working on dog mini exercise

---
+++ src/main/java/models/Dog.java
@@ -0,0 +1,66 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dogs")
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT(11) UNSIGNED NOT NULL" )
+    public long id;
+
+    @Column(columnDefinition = "TINYINT(3) UNSIGNED NOT NULL")
+    public int age;
+
+    @Column(columnDefinition = "VARCHAR(200) NOT NULL")
+    public String name;
+
+    @Column(columnDefinition = "CHAR(2) DEFAULT 'XX'")
+    public String resideState;
+
+
+    public Dog(){}
+
+    public Dog(long id, int age, String name, String resideState) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.resideState = resideState;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+
+
+}
