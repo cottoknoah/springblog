132930394e9b67af1910cd990c85236e871505db
Knoah Cotto <cotto.knoah@gmail.com>
2020-08-08 14:28:44 -0500

    understand how the controllers and models interact

--- src/main/java/com/codeup/springblog/controllers/ServiceTestController.java
+++ src/main/java/com/codeup/springblog/controllers/ServiceTestController.java
@@ -1,6 +1,10 @@
 package com.codeup.springblog.controllers;
 
+
+import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.services.EmailService;
 import com.codeup.springblog.services.GreetingSvc;
+import com.codeup.springblog.services.SillySvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +13,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ServiceTestController {
     private GreetingSvc greetingSvc;
+    private sillySvc sillySvc;
+    private EmailService emailService;
+    private PostRepository postDao;
 
-
-    public ServiceTestController(GreetingSvc greetingSvc){
+//    insert dependency injection
+    public ServiceTestController(GreetingSvc greetingSvc, SillySvc sillySvc, EmailService emailService, PostRepository postDao) {
         this.greetingSvc = greetingSvc;
+        this.sillySvc = sillySvc;
+        this.emailService = emailService;
+        this.postDao = postDao;
+    }
+
+    @GetMapping("/email")
+    @ResponseBody
+    public String sendEmail(){
+//        hard coded ID, subject, body....
+        emailService.prepareAndSend(postDao.getOne(1L), "First email!", "TESTING");
+        return "Check your mailtrap inbox!";
+    }
+
+    @GetMapping("/silly")
+    @ResponseBody
+    public int totalLength(){
+        return sillySvc.totalAdCharacters();
     }
 
     @GetMapping("/morning/{name)")
@@ -21,13 +45,13 @@ public class ServiceTestController {
         return greetingSvc.goodMorning(name);
     }
 
-    @GetMapping("/morning/{name)")
+    @GetMapping("/afternoon/{name)")
     @ResponseBody
     public String afternoonGreeting(@PathVariable String name){
         return greetingSvc.goodAfternoon(name);
     }
 
-    @GetMapping("/morning/{name)")
+    @GetMapping("/evening/{name)")
     @ResponseBody
     public String eveningGreeting(@PathVariable String name){
         return greetingSvc.goodEvening(name);
