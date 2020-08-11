029b2fd929f8d171fb4ba9d79c9e47a8b531cf1d
Knoah Cotto <cotto.knoah@gmail.com>
2020-08-11 14:49:16 -0500

    cleaning up old code

--- src/main/java/com/codeup/springblog/controllers/LoginController.java
+++ src/main/java/com/codeup/springblog/controllers/LoginController.java
@@ -1,16 +1,16 @@
-//package com.codeup.springblog.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//
-//@Controller
-//public class LoginController {
-//    @GetMapping("/login")
-//    public String viewloginForm(){
-//        return "login";
-//    }
-//
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String viewloginForm(){
+        return "login";
+    }
+
 //    @PostMapping("/login")
 //    public  String viewLoginInfo(@RequestParam(name = "username")
 //    String username, @RequestParam(name = "password") String password, Model model){
@@ -18,6 +18,6 @@
 //        model.addAttribute("password", password);
 //        return "login";
 //    }
-//
-//
-//}
+
+
+}
